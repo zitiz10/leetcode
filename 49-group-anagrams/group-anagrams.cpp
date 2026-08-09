8be174ecd30249f1013f9fdfe02de66969e2c404
Nitin Negi <nitinnegi4262@gmail.com>
2026-08-09 09:29:58 +0530

    Time: 11 ms (91.10%) | Memory: 26.1 MB (38.83%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for (auto it : mp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
