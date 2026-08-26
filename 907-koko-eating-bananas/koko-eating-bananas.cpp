80aeb2f9513294027022f412916325fab5d8eba9
Nitin Negi <nitinnegi4262@gmail.com>
2026-08-26 22:37:46 +0530

    Time: 7 ms (69.67%) | Memory: 23 MB (57.56%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -4,17 +4,18 @@ public:
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
         int ans = high;
-        while(low <= high) {
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            long long totalh = 0;
+            long long  totalh = 0;
             for (int p : piles) {
                 totalh += (p + mid - 1) / mid;
             }
             if (totalh <= h) {
                 ans = mid;
                 high = mid - 1;
-            } else
+            } else {
                 low = mid + 1;
+            }
         }
         return ans;
     }
